b2edad2c811bfce77552e8977d2e4465a455fcb9
teq-giabao <baogia@teqnological.asia>
2026-02-05 11:01:10 +0700

    fix/TRAIN-2370_fix-test-and-lint

--- httpserver/health_test.go
+++ httpserver/health_test.go
@@ -1,6 +1,7 @@
 package httpserver_test
 
 import (
+	"encoding/json"
 	"hexagon/httpserver"
 	"net/http"
 	"net/http/httptest"
@@ -10,13 +11,22 @@ import (
 )
 
 func TestHealthcheck(t *testing.T) {
-	server := httpserver.Default(testConfig())
+    server := httpserver.Default(testConfig())
 
-	req := httptest.NewRequest(http.MethodGet, "/healthcheck", nil)
-	rec := httptest.NewRecorder()
+    req := httptest.NewRequest(http.MethodGet, "/healthcheck", nil)
+    rec := httptest.NewRecorder()
 
-	server.Router.ServeHTTP(rec, req)
+    server.Router.ServeHTTP(rec, req)
 
-	assert.Equal(t, http.StatusOK, rec.Code)
-	assert.Equal(t, `{"status":"OK"}`+"\n", rec.Body.String())
+    assert.Equal(t, http.StatusOK, rec.Code)
+
+    var body map[string]interface{}
+    err := json.Unmarshal(rec.Body.Bytes(), &body)
+    assert.NoError(t, err)
+
+    assert.Equal(t, "200", body["code"])
+    assert.Equal(t, "OK", body["message"])
+
+    result := body["result"].(map[string]interface{})
+    assert.Equal(t, "OK", result["status"])
 }
